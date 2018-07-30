94c77e08f1c2a9c459e2123a6379658a5ef8745f
Aleh Dalzhenka <ortxank741@outlook.com>
2018-07-30 18:15:04 +0300

    Update2

--- Calculator/Calculator.UI/Program.cs
+++ Calculator/Calculator.UI/Program.cs
@@ -1,17 +1,33 @@
 ﻿using System;
+using System.Threading;
 using System.Diagnostics;
 
 namespace Calculator.UI
 {
     class Program
     {
+        static Mutex mutexObj = new Mutex();
+        static int x = 0;
+
         static void Main(string[] args)
         {
-            var process = Process.GetCurrentProcess();
-
-            Console.WriteLine(process.ProcessName);
+            for(int i = 0; i<5; i++)
+            {
+                var myThread = new Thread(Count);
+            }
 
             Console.ReadKey();
+        }
+
+        private static void Count()
+        {
+            mutexObj.WaitOne();
+            x = 1;
+
+            for(var i = 1; i<9; i++)
+            {
+
+            }
         }
     }
 }
