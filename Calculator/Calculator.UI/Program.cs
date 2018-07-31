44ec7ea6c1840305562a62dedf0b75033ef7abd1
Aleh Dalzhenka <ortxank741@outlook.com>
2018-07-31 16:50:03 +0300

    UseNorm

--- Calculator/Calculator.UI/Program.cs
+++ Calculator/Calculator.UI/Program.cs
@@ -1,33 +1,89 @@
 ﻿using System;
 using System.Threading;
 using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Calculator.UI
 {
-    class Program
+    public class ThreadParametr
     {
-        static Mutex mutexObj = new Mutex();
-        static int x = 0;
+        public int Index { get; set; }
 
-        static void Main(string[] args)
+        public int[,] MainArr;
+    }
+
+    public class Program
+    {
+        const int MaxRows = 4;
+
+        public static int[] Arr = new int[MaxRows];
+
+        private static void Main()
         {
-            for(int i = 0; i<5; i++)
+            var array = new[,]
             {
-                var myThread = new Thread(Count);
+                {1, 2, 2,},
+                {3, 3, 6,},
+                {7, 6, 1,},
+                {7, 6, 1,}
+            };
+
+            var threads = new Thread[MaxRows];
+
+            for (var i = 0; i < MaxRows; i++)
+            {
+                Thread.Sleep(50);
+
+                var parametrs = new ThreadParametr { Index = i,  MainArr = array };
+                threads[i] = new Thread(new ParameterizedThreadStart(Count));
+
+                threads[i].Start(parametrs);
+            }
+
+            //Console.WriteLine(array.GetRow(1));
+
+            for (var i = 0; i < Arr.Length; i++)
+            {
+                Console.WriteLine(Arr[i]);
             }
 
             Console.ReadKey();
         }
 
-        private static void Count()
+        private static void Count(object obj)
         {
-            mutexObj.WaitOne();
-            x = 1;
+                var parametrs = (ThreadParametr)obj;
 
-            for(var i = 1; i<9; i++)
+                if (Arr[parametrs.Index] != 0)
+                {
+                    throw new ArgumentException("Уже есть значение");
+                }
+
+                var num = parametrs.MainArr.GetRow(parametrs.Index);
+
+                Arr[parametrs.Index] = num;
+        }
+    }
+
+
+
+    public static class ArrayExt
+    {
+        public static int GetRow(this int[,] array, int row)
+        {
+            if (array == null)
             {
-
+                throw new ArgumentNullException("ERROR: Null Array");
             }
+
+            var cols = array.GetUpperBound(1) + 1;
+            int[] result = new int[cols];
+            var size = Marshal.SizeOf<int>();
+
+            Buffer.BlockCopy(array, row * cols * size, result, 0, cols * size);
+
+            return result.Sum();
         }
     }
 }
