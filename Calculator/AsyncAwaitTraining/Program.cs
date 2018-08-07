7aa051fd7c736116708475543d660dc23e059662
Aleh Dalzhenka <ortxank741@outlook.com>
2018-08-07 18:51:04 +0300

    End work day

---
+++ Calculator/AsyncAwaitTraining/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace AsyncAwaitTraining
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
