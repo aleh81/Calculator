54495757220c8003db3f9243701842ecbe928f9f
Oleg <User@DESKTOP-JNRD69N>
2018-10-13 18:55:47 +0300

    hello world

--- Calculator/Calculator.MVVM/ViewModels/ApplicationViewModel.cs
+++ Calculator/Calculator.MVVM/ViewModels/ApplicationViewModel.cs
@@ -14,6 +14,7 @@ using System.Diagnostics;
 
 namespace Calculator.MVVM.ViewModels
 {
+    //hello world
     class ApplicationViewModel : INotifyPropertyChanged
     {
         private bool eventOccured = false;
